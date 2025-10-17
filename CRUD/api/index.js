0af9bfb1e8d5a4c28f7cfce6115282696298def5
Diego Eiti Nakashima Fortunato <diegoeiti96@gmail.com>
2025-10-17 19:00:29 -0300

    .

--- CRUD/api/index.js
+++ CRUD/api/index.js
@@ -9,4 +9,7 @@ app.use(cors());
 
 app.use("/", userRoutes);
 
-app.listen(8800);
+const PORT = process.env.PORT || 8800;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
