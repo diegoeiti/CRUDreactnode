9e1fdf39e59070845c3c945d675eaa851d7f95e7
Diego Eiti Nakashima Fortunato <diegoeiti96@gmail.com>
2025-10-17 19:02:27 -0300

    .

--- CRUD/api/index.js
+++ CRUD/api/index.js
@@ -5,7 +5,10 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*", 
+    credentials: true
+}));
 
 app.use("/", userRoutes);
 
