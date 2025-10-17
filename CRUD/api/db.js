7d6728ed7ec87dc66d40fcedea255c7c4d622eed
Diego Eiti Nakashima Fortunato <diegoeiti96@gmail.com>
2025-10-17 20:17:37 -0300

    .

--- CRUD/api/db.js
+++ CRUD/api/db.js
@@ -1,8 +1,21 @@
-import mysql from "mysql";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "diegodev123",
-    database: "crud"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false
+});
+
+db.connect(err => {
+    if (err) {
+        console.error("Erro na conexão com o MySQL:", err);
+    } else {
+        console.log("✅ Conectado ao MySQL da Aiven!");
+    }
 });
